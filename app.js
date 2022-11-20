e47e0075013b55045f978240dcb46dfc55ef5f10
M. Mishieru <mchan324@my.bcit.ca>
2022-11-19 17:51:04 -0800

    adding in an edge case for the 74th games + pre-rec games endpoint

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.get('/', (req, res) => {
 app.get('/api/victors', getAllVictors);
 app.get('/api/victor/:id', getSpecificVictor);
 
+app.get('/api/pre_reclamation_victors', getAllPreReclamationVictors)
+
 app.get('/api/district_games_victors', getAllDistrictVictors);
 app.get('/api/district_games_victor/:id', getSpecificDistrictVictor);
 app.get('/api/district_games_victor', getTriticaMacDonaldsInfo);
@@ -30,13 +32,31 @@ function getSpecificVictor(req, res) {
   var search = req.params.id
 
   if (search == "75") {
-    return res.status(404).json({"error": "The 75th Hunger Games does not have a Victor because the Games ended prematurely."});
-  } else if (items["victors"][search]) {
+    return res.status(400).json({"error": "The 75th Hunger Games does not have a Victor because the Games ended prematurely."});
+  } else if (search == "74") {
+    let jsonObjects = {};
+    jsonObjects["74a"] = items["victors"]["74a"];
+    jsonObjects["74b"] = items["victors"]["74b"];
+    res.send(jsonObjects);
+  }
+  else if (items["victors"][search]) {
     res.send(items["victors"][search]);
   } else {
     return res.status(404).json({"error": "Not found. Please check that you entered in a year between either 1-74 or 76-100."});
   }
 
+}
+
+function getAllPreReclamationVictors(req, res) {
+  NUMBER_OF_PRE_RECLAMATION_HUNGER_GAMES = 74;
+  let jsonObjects = {};
+  
+  for (var key in items["victors"]) {
+    if (parseInt(key) < NUMBER_OF_PRE_RECLAMATION_HUNGER_GAMES || key == "74a" || key == "74b")
+    jsonObjects[key] = items["victors"][key]
+  }
+
+  res.send(jsonObjects)
 }
 
 function getAllDistrictVictors(req, res) {
